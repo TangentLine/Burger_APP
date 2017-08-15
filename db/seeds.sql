71b27d43bc7ae8cc8aaec4c7ee6b2d5aaeb56fef
TangentLine <c.jones299@gmail.com>
2017-08-15 17:31:12 -0400

    Add files via upload
    commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon + Mushroom');
+INSERT INTO  (burger_name) VALUES ('Pulled Pork');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Brisket Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Egg + Mushroom', true);
+INSERT INTO burgers (burger) VALUES ('Whooper');
